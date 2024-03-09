163fcd6a4b24fcb31113426985ac8d1a03a32b3c
Gleb Myznikov <myznikovgleb@yandex.ru>
2024-03-09 10:15:35 +0300

    Add threejs experience

--- src/ThreeDComponent.jsx
+++ src/ThreeDComponent.jsx
@@ -1,36 +1,19 @@
-import * as THREE from 'three';
-import React, { useEffect } from 'react';
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+import { Canvas } from '@react-three/fiber'
+import { useFBX, OrbitControls } from '@react-three/drei'
 
 const My3DModel = () => {
-    useEffect(() => {
-      const scene = new THREE.Scene();
-      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-      const renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      document.body.appendChild(renderer.domElement);
-  
-      const loader = new FBXLoader();
-      loader.load('3dnode.fbx', (object) => {
-        scene.add(object);
-      });
-  
-      camera.position.z = 5;
-  
-      const animate = () => {
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-      };
-  
-      animate();
-  
-      return () => {
-        document.body.removeChild(renderer.domElement);
-      };
-    }, []);
-  
-    return null;
-  };
-  
-  export default My3DModel;
+  let fbx = useFBX('3dnode.fbx')
 
+  return (
+    <div style={{ width: '1024px', height: '1024px' }}>
+      <Canvas>
+        <OrbitControls />
+        <ambientLight intensity={0.1} />
+        <directionalLight color="white" position={[0, 0, 5]} />
+        <primitive object={fbx} />
+      </Canvas>
+    </div>
+  )
+}
+
+export default My3DModel
